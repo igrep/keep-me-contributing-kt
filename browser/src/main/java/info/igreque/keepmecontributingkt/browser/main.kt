5de6cb2d10e19a0d13a988ddb5504b7f4857acad
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2019-03-13 20:49:14 +0900

    Get check target information from the form

--- browser/src/main/java/info/igreque/keepmecontributingkt/browser/main.kt
+++ browser/src/main/java/info/igreque/keepmecontributingkt/browser/main.kt
@@ -1,17 +1,27 @@
 import info.igreque.keepmecontributingkt.browser.GitHubClientJs
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import kotlin.browser.window
+import org.w3c.dom.HTMLInputElement
+import kotlin.browser.document
 
 fun main() {
-    GlobalScope.launch {
-        val query = GitHubClientJs.getQuery("/dist/GetMaster.graphql")
-        val accessToken = window.prompt("GitHub Access token") ?: ""
-        val latestCommitDate = GitHubClientJs(accessToken, query)
-            .getLatestCommitDate(
-                "igrep",
-                "daily-commits"
-            )
-        console.log("Latest commit date: $latestCommitDate")
-    }
+    document.getElementById("formCheckTarget")!!.addEventListener("submit", {
+        it.preventDefault()
+        it.stopPropagation()
+
+        val contributorName =
+            (document.getElementById("inputContributorName") as HTMLInputElement).value
+        val repositoryName =
+            (document.getElementById("inputRepositoryName") as HTMLInputElement).value
+        val accessToken =
+            (document.getElementById("inputAccessToken") as HTMLInputElement).value
+
+        GlobalScope.launch {
+            val query = GitHubClientJs.getQuery("/dist/GetMaster.graphql")
+            val latestCommitDate =
+                GitHubClientJs(accessToken, query).getLatestCommitDate(contributorName, repositoryName)
+            console.log("Latest commit date: $latestCommitDate")
+        }
+
+    })
 }
