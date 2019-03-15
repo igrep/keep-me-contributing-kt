d705dcbafa4f45903a8fff3ca6526047ac2b4905
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2019-03-15 16:13:21 +0900

    Prepend logged time on browser

--- browser/src/main/java/info/igreque/keepmecontributingkt/browser/MyLoggerJs.kt
+++ browser/src/main/java/info/igreque/keepmecontributingkt/browser/MyLoggerJs.kt
@@ -1,15 +1,16 @@
 import info.igreque.keepmecontributingkt.core.MyLogger
+import kotlin.js.Date
 
 object MyLoggerJs : MyLogger {
     override fun w(message: String) {
-        console.warn(message)
+        console.warn(Date().toString(), message)
     }
 
     override fun i(message: String) {
-        console.info(message)
+        console.info(Date().toString(), message)
     }
 
     override fun e(message: String, err: Exception) {
-        console.error(message, err)
+        console.error(Date().toString(), message, err)
     }
 }
