6c57652dc7518f4d715631cdfc9da5461b5b15a6
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2019-03-15 17:33:15 +0900

    :bug: Fix: notification Done gets popped app twice after updating the CheckTarget

--- core/src/commonMain/kotlin/info/igreque/keepmecontributingkt/core/UpdateCheckTargetInteraction.kt
+++ core/src/commonMain/kotlin/info/igreque/keepmecontributingkt/core/UpdateCheckTargetInteraction.kt
@@ -3,7 +3,7 @@ package info.igreque.keepmecontributingkt.core
 class UpdateCheckTargetInteraction(private val env: Env) {
     suspend fun run(userName: CharSequence, repositoryName: CharSequence, accessToken: CharSequence) {
         val target = CheckTarget(userName, repositoryName, accessToken, null)
-        env.contributionStatusChecker.doCheck(target)
         env.checkTargetRepository.save(target)
+        env.contributionStatusChecker.doCheck(target)
     }
 }
