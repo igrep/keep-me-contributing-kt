a389e2ebf0b4608270afe521a3dfc7b515ce65b0
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2019-03-20 21:53:53 +0900

    Try to run with sudo

--- build-kotlinjs.sh
+++ build-kotlinjs.sh
@@ -4,8 +4,8 @@ export ANDROID_COMPILE_SDK="28"
 export ANDROID_BUILD_TOOLS="28.0.2"
 export ANDROID_SDK_TOOLS="4333796"
 
-apt-get --quiet update --yes
-apt-get --quiet install --yes openjdk-8-jdk wget tar unzip lib32stdc++6 lib32z1
+sudo apt-get --quiet update --yes
+sudo apt-get --quiet install --yes openjdk-8-jdk wget tar unzip lib32stdc++6 lib32z1
 wget --quiet --output-document=android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-${ANDROID_SDK_TOOLS}.zip
 unzip -d android-sdk-linux android-sdk.zip
 echo y | android-sdk-linux/tools/bin/sdkmanager "platforms;android-${ANDROID_COMPILE_SDK}" >/dev/null
