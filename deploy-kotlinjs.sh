30be37846bd8af4c4f52c8977833a9a73af8457a
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2019-03-20 22:23:45 +0900

    Try to deploy on Netlify again
    Avoid to run apt-get to avoid permission error

--- deploy-kotlinjs.sh
+++ deploy-kotlinjs.sh
@@ -1,11 +1,8 @@
 set -eux
 
-export ANDROID_COMPILE_SDK="28"
-export ANDROID_BUILD_TOOLS="28.0.2"
-export ANDROID_SDK_TOOLS="4333796"
-
-sudo apt-get --quiet update --yes
-sudo apt-get --quiet install --yes openjdk-8-jdk wget tar unzip lib32stdc++6 lib32z1
+ANDROID_COMPILE_SDK="28"
+ANDROID_BUILD_TOOLS="28.0.2"
+ANDROID_SDK_TOOLS="4333796"
 wget --quiet --output-document=android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-${ANDROID_SDK_TOOLS}.zip
 unzip -d android-sdk-linux android-sdk.zip
 echo y | android-sdk-linux/tools/bin/sdkmanager "platforms;android-${ANDROID_COMPILE_SDK}" >/dev/null
@@ -14,6 +11,7 @@ echo y | android-sdk-linux/tools/bin/sdkmanager "build-tools;${ANDROID_BUILD_TOO
 export ANDROID_HOME=$PWD/android-sdk-linux
 export PATH=$PATH:$PWD/android-sdk-linux/platform-tools/
 chmod +x ./gradlew
+# temporarily disable checking for EPIPE error and use yes to accept all licenses
 set +o pipefail
 yes | android-sdk-linux/tools/bin/sdkmanager --licenses
 set -o pipefail
@@ -22,3 +20,4 @@ bash ./gradlew browser:runDceKotlinJs
 mkdir -p public/dist/
 cp browser/dist/*.graphql browser/dist/*.js public/dist/
 cp -r browser/{css,img,index.html} public
+netlify deploy --prod --dir=public
