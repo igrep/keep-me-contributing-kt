81f8117892a15e47cb99d2662629dae096b79299
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2019-03-20 23:05:41 +0900

    Give up to deploy on Netlify

---
+++ deploy-kotlinjs.sh
@@ -0,0 +1,7 @@
+set -eux
+
+bash ./gradlew browser:runDceKotlinJs
+mkdir -p public/dist/
+cp browser/dist/*.graphql browser/dist/*.js public/dist/
+cp -r browser/{css,img,index.html} public
+netlify deploy --prod --dir=public
